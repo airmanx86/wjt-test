c4ca966b8c77e5540c453aa7b83fcfafe764e49f
Arthur Ho <arthur_hohoho@hotmail.com>
2025-04-25 15:41:47 +1000

    Add API project with health and movies endpoints

---
+++ src/Wjt.Api/Program.cs
@@ -0,0 +1,32 @@
+using Wjt.CinemaWorld.Config;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddOpenApi()
+    .AddCinemaWorldService(builder.Configuration.GetSection("CinemaWorldApi").Get<CinemaWorldApiOptions>());
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/health", () => "I am alive!");
+
+app.MapGet("/api/movies", async (ICinemaWorldService cinemaWorldService) =>
+{
+    var (movies, response) = await cinemaWorldService.GetMoviesAsync();
+
+    if (response.IsSuccessStatusCode)
+    {
+        return Results.Ok(movies);
+    }
+
+    return Results.Problem("Error fetching movies");
+});
+
+app.Run();
