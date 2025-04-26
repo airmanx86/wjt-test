2a6d5177cca62a8a0b37ca471ee2541a7d2841e8
Arthur Ho <arthur_hohoho@hotmail.com>
2025-04-26 13:29:05 +1000

    Fix FilmWorldService deserialization error

--- src/Wjt.FilmWorld/FilmWorldService.cs
+++ src/Wjt.FilmWorld/FilmWorldService.cs
@@ -9,16 +9,16 @@ public class FilmWorldService(HttpClient httpClient) : IFilmWorldService
 
     private const string MovieDetailsEndpoint = "/api/filmworld/movie/";
 
-    private readonly HttpClient _httpClient = httpClient;
+    private readonly JsonSerializerOptions jsonSerializerOptions = new(JsonSerializerDefaults.Web);
 
     public async Task<(MoviesResponse?, HttpResponseMessage)> GetMoviesAsync()
     {
-        var response = await _httpClient.GetAsync(MoviesEndpoint);
+        var response = await httpClient.GetAsync(MoviesEndpoint);
 
         if (response.IsSuccessStatusCode)
         {
             using var stream = await response.Content.ReadAsStreamAsync();
-            var movieResponse = await JsonSerializer.DeserializeAsync<MoviesResponse>(stream);
+            var movieResponse = await JsonSerializer.DeserializeAsync<MoviesResponse>(stream, jsonSerializerOptions);
             return (movieResponse, response);
         }
         else
@@ -29,12 +29,12 @@ public class FilmWorldService(HttpClient httpClient) : IFilmWorldService
 
     public async Task<(MovieDetails?, HttpResponseMessage)> GetMovieDetailsAsync(string id)
     {
-        var response = await _httpClient.GetAsync(MovieDetailsEndpoint + Uri.EscapeDataString(id));
+        var response = await httpClient.GetAsync(MovieDetailsEndpoint + Uri.EscapeDataString(id));
 
         if (response.IsSuccessStatusCode)
         {
             using var stream = await response.Content.ReadAsStreamAsync();
-            var movieDetails = await JsonSerializer.DeserializeAsync<MovieDetails>(stream);
+            var movieDetails = await JsonSerializer.DeserializeAsync<MovieDetails>(stream, jsonSerializerOptions);
             return (movieDetails, response);
         }
         else
